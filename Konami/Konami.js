0e30aeefe8791ee1cc908bf8b1f29f2853a094ce
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-09-05 16:54:42 -0500

    restored mobile functionality

--- Konami/Konami.js
+++ Konami/Konami.js
@@ -1,4 +1,5 @@
 "USE STRICT";
+
 let userInput = "";	
 const seq = "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter";
 const soundEffect1 = document.getElementById("sfx");
@@ -13,20 +14,81 @@ const fireKey = document.getElementById('select-button');
 let controller = document.getElementById("controller");
 let pressStart = document.querySelector("#pressStart");
 let img = document.getElementById("img");
-leftArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'ArrowLeft'}))});
-upArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'ArrowUp'}))});
-downArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'ArrowDown'}))});
-rightArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'ArrowRight'}))});
-aKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'a'}))});
-bKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'b'}))});
-startKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'Enter'}))});
-fireKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keydown", {'key': 'Space'}))});
+
+leftArrowKey.addEventListener("touchend", convertToLeft_mobile);
+leftArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'ArrowLeft'}))});
+
+upArrowKey.addEventListener("touchend", convertToUp_mobile);
+upArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'ArrowUp'}))});
+
+downArrowKey.addEventListener("touchend", convertToDown_mobile);
+downArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'ArrowDown'}))});
+
+rightArrowKey.addEventListener("touchend", convertToRight_mobile);
+rightArrowKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'ArrowRight'}))});
+
+aKey.addEventListener("touchend", convertToA_mobile);
+aKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'a'}))});
+
+bKey.addEventListener("touchend", convertToB_mobile);
+bKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'b'}))});
+
+startKey.addEventListener("touchend", convertToEnter_mobile);
+startKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'Enter'}))});
+
+// fireKey.addEventListener("touchend", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'Space'}))});
+fireKey.addEventListener("click", () => {body.dispatchEvent(new KeyboardEvent("keyup", {'key': 'Space'}))});
+
+document.addEventListener("touchend", function (e){
+    if(userInput.includes(seq)){
+        soundEffect1.play();
+        img.classList.toggle("hidden");
+        userInput="";
+    }
+});
 document.addEventListener("keyup", checkCode);
+
 pressStart.addEventListener("click", clickToShowController);
+
+function convertToLeft_mobile(){
+    userInput+="ArrowLeft"
+    console.log(userInput);
+}
+function convertToRight_mobile(){
+    userInput+="ArrowRight"
+    console.log(userInput);
+}
+function convertToUp_mobile(){
+    userInput+="ArrowUp"
+    console.log(userInput);
+}
+function convertToDown_mobile(){
+    userInput+="ArrowDown"
+    console.log(userInput);
+}
+function convertToEnter_mobile(){
+    userInput+="Enter"
+    console.log(userInput);
+}
+function convertToA_mobile(){
+    userInput+="a"
+    console.log(userInput);
+}
+function convertToB_mobile(){
+    userInput+="b"
+    console.log(userInput);
+}
+
+
 function clickToShowController(){
 	console.log("clicked");
     controller.classList.toggle("hidden");
 }
+// function eventHandler(event, selector) {
+//     event.stopPropagation(); // Stop event bubbling.
+//     event.preventDefault(); // Prevent default behaviour
+//     if (event.type === 'touchend') selector.off('click'); // If event type was touch turn off clicks to prevent phantom clicks.
+// }
 function checkCode() {
 	userInput+=event.key;
     console.log(userInput);
